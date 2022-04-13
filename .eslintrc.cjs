2478b131eb3f48450f5de3bd9525c60ba4db8b95
David Cooper <dazecoop@gmail.com>
2022-04-13 10:00:07 +0100

    Install of Tailwind

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 /* eslint-env node */
-// require('@rushstack/eslint-patch/modern-module-resolution');
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
@@ -9,13 +9,12 @@ module.exports = {
     'plugin:vue/recommended',
   ],
   env: {
-    // 'vue/setup-compiler-macros': true,
+    'vue/setup-compiler-macros': true,
   },
   rules: {
     quotes: ['error', 'single'], // Prefer single quotes where possible
     'indent': ['error', 2], // Enforce indentation to 2 space-tabs
     'vue/html-self-closing': ['error'], // Prefer self-closing tags
     'comma-dangle': ['error', 'always-multiline'], // Always put a comma at the end of a multiline
-    'vue/script-setup-uses-vars': ['off'],
   },
 };
